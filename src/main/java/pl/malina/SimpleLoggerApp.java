0a1fe60a8978a2792eb05e2d44fc9f59faa0ae15
Dawid Malinowski <dawidmalina@gmail.com>
2017-02-23 14:21:47 +0100

    Add prometheus metrics

--- src/main/java/pl/malina/SimpleLoggerApp.java
+++ src/main/java/pl/malina/SimpleLoggerApp.java
@@ -2,9 +2,13 @@ package pl.malina;
 
 import java.util.Map;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.prometheus.client.exporter.MetricsServlet;
+import io.prometheus.client.hotspot.DefaultExports;
 import pl.malina.audit.SimpleAuditLogger;
 import pl.malina.nasted.NastedLogger;
 
@@ -12,14 +16,7 @@ public class SimpleLoggerApp {
 
     private final static Logger logger = LoggerFactory.getLogger(SimpleLoggerApp.class);
 
-    public static void main(String[] args) {
-
-        Integer sleep = 1000;
-        Map<String, String> env = System.getenv();
-        if (env.containsKey("SLEEP")) {
-            sleep = Integer.valueOf(env.get("SLEEP"));
-        }
-
+    private static void generateLogs(Integer sleep) {
         while (true) {
             try {
 
@@ -45,7 +42,24 @@ public class SimpleLoggerApp {
                 e.printStackTrace();
             }
         }
+    }
 
+    public static void main(String[] args) throws Exception {
+
+        Integer sleep = 1000;
+        Map<String, String> env = System.getenv();
+        if (env.containsKey("SLEEP")) {
+            sleep = Integer.valueOf(env.get("SLEEP"));
+        }
+
+        DefaultExports.initialize();
+        Server server = new Server(1080);
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+        handler.addServletWithMapping(MetricsServlet.class, "/metrics");
+        server.start();
+
+        generateLogs(sleep);
     }
 
 }
